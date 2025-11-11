5a8e3bbed620d77291c01ba2b5329a3e7de5def8
USER <USER@DESKTOP-N3KNHS7>
2025-11-11 04:42:41 +0200

    try deployed  ads on my site2

--- src/app/Core/environments/environment.prod.ts
+++ src/app/Core/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // apiUrl: 'https://masjidstory-001-site1.mtempurl.com',
-  apiUrl: 'http://ahmedmohamedasp-001-site1.anytempurl.com',
+  apiUrl: 'https://ahmedmohamedasp-001-site1.anytempurl.com',
 };
