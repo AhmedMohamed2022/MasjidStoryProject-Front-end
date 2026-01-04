454dfbdb4d3346c5bcf5fcbe09bb980dd788814f
USER <USER@DESKTOP-N3KNHS7>
2026-01-04 02:49:11 +0200

    update env for sommee backend

--- src/app/Core/environments/environment.prod.ts
+++ src/app/Core/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   // apiUrl: 'https://masjidstory-001-site1.mtempurl.com',
-  apiUrl: 'https://ahmedmohamedasp-001-site1.anytempurl.com',
+  // apiUrl: 'https://ahmedmohamedasp-001-site1.anytempurl.com',
+  apiUrl: 'http://www.masjidstory.somee.com',
 };
